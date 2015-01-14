28ede0d3a6d7e704d4f88864ae7d8008ddecab87
Cartowsky <mic.ferretti@gmail.com>
2015-01-14 23:01:39 +0100

    fixed postgresql connection & insert
    Next step is to implement a better error handling method + check the correct architecture and workflow of the code.

--- pj-porting-flask/postgres.py
+++ pj-porting-flask/postgres.py
@@ -1,77 +1,63 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# import os
-# import sys
-# import psycopg2
-# from flask import Flask
-# from flask import g
-# from flask import request, redirect
-# from flask import render_template
-# from flask.ext.sqlalchemy import SQLAlchemy
-# import json
-# # import sqlite3
 
+# SQL command to create a table 
+# CREATE TABLE shapes (id integer, data json);
 
-
-# db_conn = 'postgresql+psycopg2://databaseuser:P@ssw0rd@localhost/the_database'
-db_conn = 'postgresql+psycopg2://mic:@localhost/milanography'
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = db_conn
-db = SQLAlchemy(app)
-
-@app.before_request
-def before_request():
-    # print SQLAlchemy(app)
-    # db = SQLAlchemy(app)
-
-# @app.teardown_request
-# def teardown_request(exception):
-#     if hasattr(db, 'db'):
-#         db.close()
-
-# @app.route('/save_json', methods = ['POST'])
-# def save_json():
-#     data = request.form['data']
-#     db.execute("INSERT INTO shapes VALUES (?)", [data])
-#     db.commit()
-#     print "salvo data"
-#     print data
-#     return redirect('/')
-
-# @app.route('/', methods = ['GET'])
-# def print_prova():
-#     print "funziona!"
-#     return render_template('index.html')
-
-# if __name__ == '__main__':
-#   app.run(debug=True)
-
-
-# SQL command to create table
-CREATE TABLE shapes (id integer, data json);
-
-# script funzionante
 import psycopg2
 import sys
+from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask import request, redirect
+from flask import render_template
+from flask.ext.sqlalchemy import SQLAlchemy
 
 
-con = None
+app = Flask(__name__)
 
-try:
-     
-    con = psycopg2.connect(database='milanography', user='mic') 
-    cur = con.cursor()
-    cur.execute('SELECT version(); ')          
-    ver = cur.fetchone()
-    print ver    
-    
+db_conn = 'postgresql+psycopg2://mic:@localhost/milanography'
+app.config['SQLALCHEMY_DATABASE_URI'] = db_conn
 
-except psycopg2.DatabaseError, e:
-    print 'Error %s' % e    
-    sys.exit(1)
-    
-    
-finally:
-    
-    if con:
-        con.close()
+con = psycopg2.connect(database='milanography', user='mic') 
+cur = con.cursor()
+
+# is this part still useful?
+# @app.before_request
+# def before_request():
+#     try:
+#         cur.connect()
+#         cur.execute('SELECT version(); ')          
+#         ver = cur.fetchone()
+#         print ver 
+#     except psycopg2.DatabaseError, e:
+#         print 'Error %s' % e    
+#         sys.exit(1) 
+
+# @app.teardown_request
+# def teardown_request(exception):      
+#     con.close()
+
+# good explanation  of the following code:
+# https://stackoverflow.com/questions/5342698/cursor-executeinsert-into-im-entry-test-entrym-values-p
+@app.route('/save_json', methods = ['POST'])
+def save_json():    
+    data = request.form['data']
+    if data != '':
+        cur.execute('INSERT INTO shapes (data) VALUES (%s)', [data])
+        con.commit() 
+        print "salvo data"
+        print data
+        return redirect('/')
+    else:
+        print "Non ho dati da salvare!"
+        print data
+        return redirect('/')
+
+@app.route('/', methods = ['GET'])
+def print_prova():
+    print "funziona!"
+    return render_template('index.html')
+
+if __name__ == '__main__':
+  # TODO remove debbuger in production!
+  app.run(debug=True)
